9e5d00dd4f9991afc118658ab8128903bf934740
alruwail023 <alruwail023@gannon.edu>
2023-12-06 22:28:12 -0500

    Updated by Mohammed

--- backend/src/UserApi/Entities/User.cs
+++ backend/src/UserApi/Entities/User.cs
@@ -1,33 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace UserApi.Entities;
-
-public partial class User
+namespace UserApi.Entities
 {
-    public Guid Id { get; set; }
+    public partial class User
+    {
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public byte[] PasswordHash { get; set; } = null!;
+        public byte[] PasswordSalt { get; set; } = null!;
+        public string? Role { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateModified { get; set; }
+        public DateTime? LastLogin { get; set; }
+        public int? FailedLoginAttempts { get; set; }
 
-    public string? FirstName { get; set; }
+        // existing properties
 
-    public string? LastName { get; set; }
-
-    public string Username { get; set; } = null!;
-
-    public string Email { get; set; } = null!;
-
-    public byte[] PasswordHash { get; set; } = null!;
-
-    public byte[] PasswordSalt { get; set; } = null!;
-
-    public string? Role { get; set; }
-
-    public bool? IsActive { get; set; }
-
-    public DateTime? DateCreated { get; set; }
-
-    public DateTime? DateModified { get; set; }
-
-    public DateTime? LastLogin { get; set; }
-
-    public int? FailedLoginAttempts { get; set; }
+        public string? TwoFactorSecretKey { get; set; }
+    }
 }
--- backend/src/UserApi/Services/UserService.cs
+++ backend/src/UserApi/Services/UserService.cs
@@ -5,73 +5,72 @@ using UserApi.Helpers;
 using UserApi.Models;
 using UserApi.Repositories;
 
-namespace UserApi.Services;
-
-public class UserService : IUserService
+namespace UserApi.Services
 {
-    private readonly IUserRepository _userRepository;
-    private readonly IJwtUtils _jwtUtils;
-    private readonly IMapper _mapper;
-    private readonly IPasswordHasher _passwordHasher;
-
-
-    public UserService(IUserRepository userRepository, IJwtUtils jwtUtils, IMapper mapper, IPasswordHasher passwordHasher)
+    public class UserService : IUserService
     {
-        _userRepository = userRepository;
-        _jwtUtils = jwtUtils;
-        _mapper = mapper;
-        _passwordHasher = passwordHasher;
-    }
+        private readonly IUserRepository _userRepository;
+        private readonly IJwtUtils _jwtUtils;
+        private readonly IMapper _mapper;
+        private readonly IPasswordHasher _passwordHasher;
 
-    public async Task<AuthenticateResponse?> Authenticate(AuthenticateRequest model)
-    {
-        // get user from database
-        var user = await _userRepository.GetUserByUsernameAsync(model.Username);
+        public UserService(IUserRepository userRepository, IJwtUtils jwtUtils, IMapper mapper, IPasswordHasher passwordHasher)
+        {
+            _userRepository = userRepository;
+            _jwtUtils = jwtUtils;
+            _mapper = mapper;
+            _passwordHasher = passwordHasher;
+        }
 
-        // return null if user not found
-        if (user == null) return null;
+        public async Task<AuthenticateResponse?> Authenticate(AuthenticateRequest model)
+        {
+            // get user from database
+            var user = await _userRepository.GetUserByUsernameAsync(model.Username);
 
-        // check if the provided password matches the password in the database and return null if it doesn't
-        if (!_passwordHasher.ValidatePassword(model.Password, user.PasswordHash, user.PasswordSalt)) return null;
+            // return null if user not found
+            if (user == null) return null;
 
-        // authentication successful so generate jwt token
-        var token = _jwtUtils.GenerateJwtToken(user);
+            // check if the provided password matches the password in the database and return null if it doesn't
+            if (!_passwordHasher.ValidatePassword(model.Password, user.PasswordHash, user.PasswordSalt)) return null;
 
-        
-        // map user and token to response model with Automapper and return
-        return _mapper.Map<AuthenticateResponse>(user, opts => opts.Items["Token"] = token);
-    }
+            // authentication successful so generate jwt token
+            var token = _jwtUtils.GenerateJwtToken(user);
 
-    public async Task<CreateUserResponse?> CreateUserAsync(CreateUserRequest userRequest)
-    {
-        // Hash and salt the password
-        (byte[] passwordHash, byte[] passwordSalt) = _passwordHasher.HashPassword(userRequest.Password);
+            // map user and token to response model with Automapper and return
+            return _mapper.Map<AuthenticateResponse>(user, opts => opts.Items["Token"] = token);
+        }
 
-        // Map CreateUserRequest model to User entity with Automapper
-        var userEntity = _mapper.Map<User>(userRequest);
+        public async Task<CreateUserResponse?> CreateUserAsync(CreateUserRequest userRequest)
+        {
+            // Hash and salt the password
+            (byte[] passwordHash, byte[] passwordSalt) = _passwordHasher.HashPassword(userRequest.Password);
 
-        // Assign hashed and salted password to user entity
-        userEntity.PasswordHash = passwordHash;
-        userEntity.PasswordSalt = passwordSalt;
+            // Map CreateUserRequest model to User entity with Automapper
+            var userEntity = _mapper.Map<User>(userRequest);
 
-        // Create user in database
-        var createdUser = await _userRepository.CreateUserAsync(userEntity)
-            ?? throw new Exception("An error occurred when creating user. Try again later.");
+            // Assign hashed and salted password to user entity
+            userEntity.PasswordHash = passwordHash;
+            userEntity.PasswordSalt = passwordSalt;
 
-        // Map User entity to CreateUserResponse model with Automapper
-        return _mapper.Map<CreateUserResponse>(createdUser);
-    }
+            // Create user in database
+            var createdUser = await _userRepository.CreateUserAsync(userEntity)
+                ?? throw new Exception("An error occurred when creating user. Try again later.");
 
-    public async Task<IEnumerable<UserResponse>> GetAllAsync()
-    {
-        var users = await _userRepository.GetAllUsersAsync();
-    
-        return _mapper.Map<IEnumerable<UserResponse>>(users);
-    }
+            // Map User entity to CreateUserResponse model with Automapper
+            return _mapper.Map<CreateUserResponse>(createdUser);
+        }
 
-    public async Task<UserResponse?> GetByIdAsync(string id)
-    {
-        var user = await _userRepository.GetUserByIdAsync(id);
-        return _mapper.Map<UserResponse>(user);
+        public async Task<IEnumerable<UserResponse>> GetAllAsync()
+        {
+            var users = await _userRepository.GetAllUsersAsync();
+
+            return _mapper.Map<IEnumerable<UserResponse>>(users);
+        }
+
+        public async Task<UserResponse?> GetByIdAsync(string id)
+        {
+            var user = await _userRepository.GetUserByIdAsync(id);
+            return _mapper.Map<UserResponse>(user);
+        }
     }
 }
